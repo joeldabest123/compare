6219e943352a763a2963b66101a8d4cfeeea109a
Joel Anderson <joeleverett@hotmail.com>
2026-03-29 19:09:40 -0400

    Complete paceDirectories and am now working on tokenize. Kill me please

--- tokenizer.c
+++ tokenizer.c
@@ -7,59 +7,59 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include "tokenizer.h"
+#include "linkedList.h"
+
 
 //something like if addToCount == -1, insert new Node
 
 //reads from file, then calls add to count. 
 
-void openFile(const char *file) {
-    int fd = open(file, O_RDONLY);
-    //
-
-}
-
 
 void paceDirectories(const char *path) {
     DIR* d = opendir(path);
+
+    if(d == NULL) {
+        perror("Error opening Directory");
+        return;
+    }
+
     struct dirent* entry = readdir(d);
     int bufferCount = 0;
-    char* fileName = entry->d_name;
+    char* fileName;
 
-
-    
     while(entry != NULL) {
 
         fileName = entry->d_name;
-        
         if(strcmp(fileName, ".") == 0 || strcmp(fileName, "..") == 0) {
             entry = readdir(d);
-            fileName = entry->d_name;
             continue;
         }
 
-        int entryLength = strlen(fileName);
+        char* fullPath = bufferBuilder(path, entry);
+        struct stat st;
 
-        char* suffix = fileName + (entryLength - 4);
-        if(strcmp(suffix, ".txt") == 0) {
-            char* fullPath = bufferBuilder(path, entry);
+        stat(fullPath, &st);
+        if(S_ISREG(st.st_mode)) {
+            int entryLength = strlen(fileName);
+            if(entryLength >= 4) {
+                char* suffix = fileName + (entryLength - 4);
 
-            struct stat st;
-
-            stat(fullPath, &st);
-            if(S_ISREG(st.st_mode)) {
-                openFile(fullPath);
-            } else {
+                if(strcmp(suffix, ".txt") == 0) {
+                    tokenize(fullPath);
+                };
+            }
+        } else {
+            if (S_ISDIR(st.st_mode)) {
                 paceDirectories(fullPath);
             }
-            
-            free(fullPath);
-
         }
+
+        free(fullPath);
         entry = readdir(d);
 
     }
 
-    close(d);
+    closedir(d);
 }
 
 char* bufferBuilder(const char *path, struct dirent* entry) {
@@ -74,8 +74,19 @@ char* bufferBuilder(const char *path, struct dirent* entry) {
     return tempBuffer;
 }
 
-void tokenize(const char *path) {
-    
+void tokenize(const char *file) {
+    int fd = open(file, O_RDONLY);
+
+    if(fd == -1) {
+        perror("Error opening file");
+        return;
+    }
+
+    while()
+
+    if()
+
+    close(file);
 }
 
 //int fd = open(path, O_RDONLY);
