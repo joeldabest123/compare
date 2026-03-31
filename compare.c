297518bd969a933d41ff861c83cf81c60d648ead
Joel Anderson <joeleverett@hotmail.com>
2026-03-30 21:44:04 -0400

    Worked out kinks in main

--- compare.c
+++ compare.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdint.h>
+#include <sys/stat.h>
 #include "linkedList.h"
 #include "tokenizer.h"
 #include "math.h"
@@ -19,9 +20,33 @@ int main (int argc, char* argv[]) {
     //Setting up my glorious Master Array
     List ** allFiles = initializeArray();
 
-    //runs through directories and files to get to the lowest files
-    //Then reads said files and writes them to the master allfiles
-    paceDirectories(argv[1], &allFiles, &fileCount, &capacity);
+    //loops through all user inputs to take multiple arguments
+    for (int i = 1; i < argc; i++) {
+        struct stat arg_st;
+        if(stat(argv[i], &arg_st) != 0) {
+            perror("Error accessing arg");
+            continue;
+        }
+
+        if (S_ISDIR(arg_st.st_mode)) {
+
+            //runs through directories and files to get to the lowest files
+            //Then reads said files and writes them to the master allfiles
+            paceDirectories(argv[i], allFiles, &fileCount, &capacity);
+        } else if (S_ISREG(arg_st.st_mode)) {
+            //checks if more space is needed for stuff in master array
+            if(fileCount >= capacity) {
+                allFiles = lengthenArray(allFiles, &capacity);
+            }
+
+            //initializes, tokenizes, and stores
+            List* newList = initializeList(argv[i]);
+            tokenize(argv[i], arg_st.st_size, newList);
+
+            allFiles[fileCount] = newList;
+            fileCount++;
+        }
+    }
 
     if(fileCount < 2) {
         printf("Error: Found %d .txt files. Need at least 2 for comparison.\n", fileCount);
